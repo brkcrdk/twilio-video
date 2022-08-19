d458f6e8b2fa379783b943c1b18cd84ac0b38e96
Burak <burak@90pixel.com>
2022-08-19 16:12:34 +0300

    Add current video element

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,30 @@
+import { useState, useEffect, useRef } from 'react';
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
+
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  return <div className={styles.container}>test</div>;
+  const [myStream, setMyStream] = useState<MediaStream | null>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const getMediaDevices = async () => {
+      const streams = await navigator.mediaDevices.getUserMedia({
+        video: true,
+      });
+      setMyStream(streams);
+      if (videoRef?.current) {
+        videoRef.current.srcObject = streams;
+      }
+    };
+    getMediaDevices();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {myStream && <video ref={videoRef} muted autoPlay playsInline />}
+    </div>
+  );
 };
 
 export default Home;
