3709fc6ae7ca7d4e3c60f4ca822976488c9bfd1b
Burak <burak@90pixel.com>
2022-08-20 17:01:54 +0300

    Redirect room with room name.

--- client/modules/Room.tsx
+++ client/modules/Room.tsx
@@ -1,9 +1,36 @@
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { connect } from 'twilio-video';
+import Video from 'twilio-video';
 
 function Room() {
   const {
-    query: { roomName },
+    query: { token, roomName },
   } = useRouter();
-  return <div>join this room {roomName}</div>;
+
+  useEffect(() => {
+    const connectRoom = async () => {
+      if (token) {
+        const room = await connect(String(token), {
+          name: String(roomName),
+          video: true,
+          audio: false,
+        });
+        const videoContainer = document.getElementById('video-container');
+
+        const participantDiv = document.createElement('div');
+        participantDiv.setAttribute('id', room.localParticipant.identity);
+        videoContainer?.appendChild(participantDiv);
+        room.localParticipant.tracks.forEach(track => {
+          // const test = new MediaStream(track.track.);
+          // return track.attach();
+          // return console.log(track);
+        });
+      }
+    };
+    connectRoom();
+  }, []);
+
+  return <div id="video-container">join room with this {token}</div>;
 }
 export default Room;
