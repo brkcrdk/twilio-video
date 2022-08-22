70f50dfb7f53cfdad7985e2c6363b2e2a5533d2b
Burak <burak@90pixel.com>
2022-08-22 17:14:20 +0300

    Update logic at room component.

--- client/modules/Room.tsx
+++ client/modules/Room.tsx
@@ -1,67 +1,71 @@
 import { useEffect, useState, useRef } from 'react';
 import { useRouter } from 'next/router';
 import { connect, LocalParticipant, Room as RoomType } from 'twilio-video';
+import { useRoom } from 'store';
 
 function Room() {
-  const [room, setRoom] = useState<RoomType | null>(null);
-  const localRef = useRef<HTMLVideoElement>(null);
-  const [localParticipant, setLocalParticipant] =
-    useState<LocalParticipant | null>(null);
-  const {
-    query: { token, roomName },
-  } = useRouter();
+  const { state } = useRoom();
 
-  useEffect(() => {
-    const connectRoom = async () => {
-      if (token && room?.state !== 'connected') {
-        const roomResponse = await connect(String(token), {
-          name: String(roomName),
-          video: true,
-          audio: false,
-        });
+  // const [room, setRoom] = useState<RoomType | null>(null);
+  // const localRef = useRef<HTMLVideoElement>(null);
+  // const [localParticipant, setLocalParticipant] =
+  //   useState<LocalParticipant | null>(null);
+  // const {
+  //   query: { token, roomName },
+  // } = useRouter();
 
-        setRoom(roomResponse);
-        setLocalParticipant(roomResponse?.localParticipant);
-      }
-    };
-    connectRoom();
-  }, []);
+  // useEffect(() => {
+  //   const connectRoom = async () => {
+  //     if (token && room?.state !== 'connected') {
+  //       const roomResponse = await connect(String(token), {
+  //         name: String(roomName),
+  //         video: true,
+  //         audio: false,
+  //       });
 
-  useEffect(() => {
-    if (localParticipant) {
-      localParticipant.videoTracks.forEach(val => {
-        if (localRef?.current) {
-          val.track.attach(localRef?.current);
-        }
-      });
-    }
-    // if (room) {
-    //   const videoContainer = document.getElementById('video-container');
-    //   const participantDiv = document.createElement('div');
-    //   participantDiv.setAttribute('id', room.localParticipant.identity);
-    //   videoContainer?.appendChild(participantDiv);
+  //       setRoom(roomResponse);
+  //       setLocalParticipant(roomResponse?.localParticipant);
+  //     }
+  //   };
+  //   connectRoom();
+  // }, []);
 
-    // }
-  }, [localParticipant]);
+  // useEffect(() => {
+  //   if (localParticipant) {
+  //     localParticipant.videoTracks.forEach(val => {
+  //       if (localRef?.current) {
+  //         val.track.attach(localRef?.current);
+  //       }
+  //     });
+  //   }
+  //   // if (room) {
+  //   //   const videoContainer = document.getElementById('video-container');
+  //   //   const participantDiv = document.createElement('div');
+  //   //   participantDiv.setAttribute('id', room.localParticipant.identity);
+  //   //   videoContainer?.appendChild(participantDiv);
 
-  useEffect(() => {
-    return () => {
-      if (room) {
-        // room.localParticipant.videoTracks.forEach(track => track.unpublish());
-        room.disconnect();
-        // room.on('participantDisconnected', (participant: Participant) => {
-        //   participant.removeAllListeners();
-        //   const participantDiv = document.getElementById(participant.identity);
-        //   participantDiv?.remove();
-        // });
-      }
-    };
-  }, [room]);
+  //   // }
+  // }, [localParticipant]);
+
+  // useEffect(() => {
+  //   return () => {
+  //     if (room) {
+  //       // room.localParticipant.videoTracks.forEach(track => track.unpublish());
+  //       room.disconnect();
+  //       // room.on('participantDisconnected', (participant: Participant) => {
+  //       //   participant.removeAllListeners();
+  //       //   const participantDiv = document.getElementById(participant.identity);
+  //       //   participantDiv?.remove();
+  //       // });
+  //     }
+  //   };
+  // }, [room]);
 
   return (
     <div id="video-container">
-      join room with this {token}
-      {localParticipant && <video ref={localRef} autoPlay muted playsInline />}
+      {/* join room with this {token} */}
+      {/* {localParticipant && <video ref={localRef} autoPlay muted playsInline />} */}
+      {JSON.stringify(state.room, null, 4)}
     </div>
   );
 }
