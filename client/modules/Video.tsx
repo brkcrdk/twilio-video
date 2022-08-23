e98130702a5cf07ad2d5d80233247e8beb6b2070
Burak <burak@90pixel.com>
2022-08-23 23:40:28 +0300

    Add styles to Video element.

--- client/modules/Video.tsx
+++ client/modules/Video.tsx
@@ -1,17 +1,33 @@
 import { forwardRef } from 'react';
 import { Participant } from 'twilio-video';
+import styles from 'styles/Home.module.css';
 
 interface VideoProps {
   participant: Participant;
+  hasVideo?: boolean;
+  isLoading?: boolean;
 }
 
 const Video = forwardRef<HTMLVideoElement, VideoProps>(
-  ({ participant }, ref) => {
+  ({ participant, hasVideo, isLoading }, ref) => {
     return (
-      <>
-        <video ref={ref} autoPlay playsInline />
-        <span>{participant.identity}</span>
-      </>
+      <div className={styles.videoContainer}>
+        <video
+          className={styles.videoElement}
+          style={{
+            display: hasVideo ? 'flex' : 'none',
+          }}
+          ref={ref}
+          autoPlay
+          playsInline
+        />
+        {/* {!hasVideo && (
+        )} */}
+        <div style={{ height: '100%' }}>
+          {isLoading ? 'Kamera Açılıyor' : 'Kamera yok'}
+        </div>
+        {/* <span>{participant.identity}</span> */}
+      </div>
     );
   }
 );
